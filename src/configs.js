f5029f3f3b6164aafd31e0597908447b01b319c0
Loouis Low <loouis@gmail.com>
2021-05-07 21:18:33 +0800

    clean up

--- src/configs.js
+++ src/configs.js
@@ -1,6 +1,12 @@
 export const colors = [
-  { modifier: 'red-100', value: '#343434' },
-  { modifier: 'red-200', value: '#141414' },
-  { modifier: 'red-300', value: '#434343' },
+  { modifier: 'gray-100', value: '#f7fafc' },
+  { modifier: 'gray-200', value: '#edf2f7' },
+  { modifier: 'gray-300', value: '#e2e8f0' },
+  { modifier: 'gray-400', value: '#cbd5e0' },
+  { modifier: 'gray-500', value: '#a0aec0' },
+  { modifier: 'gray-600', value: '#718096' },
+  { modifier: 'gray-700', value: '#4a5568' },
+  { modifier: 'gray-800', value: '#2d3748' },
+  { modifier: 'gray-900', value: '#1a202c' }
 ]
 
